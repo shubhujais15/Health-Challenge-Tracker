e4e9b4ec08a6a07b07e2a3763d8340c03d61e5ba
shubhujais15 <shubhujaisedu15@gmail.com>
2024-07-18 23:48:57 +0530

    created addWorkout function to add user

--- src/app/components/add-workout/add-workout.component.ts
+++ src/app/components/add-workout/add-workout.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { WorkoutService } from '../../services/workout.service';
 
 @Component({
   selector: 'app-add-workout',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-workout.component.html',
-  styleUrl: './add-workout.component.css'
+  styleUrls: ['./add-workout.component.css']
 })
 export class AddWorkoutComponent {
+  username = '';
+  workoutType = '';
+  workoutMinutes: number | null = null;
 
+  constructor(private workoutService: WorkoutService) {}
+
+  addWorkout() {
+    if (this.username && this.workoutType && this.workoutMinutes) {
+      const newWorkout = {
+        username: this.username,
+        workoutType: this.workoutType,
+        workoutMinutes: this.workoutMinutes
+      };
+      this.workoutService.addWorkout(newWorkout);
+
+      // Reset the form
+      this.username = '';
+      this.workoutType = '';
+      this.workoutMinutes = null;
+    }
+  }
 }
