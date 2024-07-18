7cd4e6128171b89d53fbdf943b5ae8c54945bfa0
shubhujais15 <shubhujaisedu15@gmail.com>
2024-07-19 02:30:52 +0530

    added routes for all components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AddWorkoutComponent } from './components/add-workout/add-workout.component';
+import { WorkoutListComponent } from './components/workout-list/workout-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { WorkoutProgressComponent } from './components/workout-progress/workout-progress.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent
+    },
+    {
+        path:'add-workout',
+        component: AddWorkoutComponent
+    },
+    {
+        path:'workout-list',
+        component: WorkoutListComponent
+    },
+    {
+        path: 'workout-progress',
+        component: WorkoutProgressComponent
+    }
+];
