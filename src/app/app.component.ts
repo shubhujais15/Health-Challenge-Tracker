47b9f583f0f0da720835f5bc07231da9428989ba
shubhujais15 <shubhujaisedu15@gmail.com>
2024-07-18 18:18:17 +0530

    ngOnInit to use RxJS for filter navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Health-Challenge-Tracker';
+
+  showNav = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        console.log('NavigationEnd:', event.urlAfterRedirects);
+        this.showNav = event.urlAfterRedirects !== '/';
+        console.log('Show Nav:', this.showNav);
+      });
+  }
 }
