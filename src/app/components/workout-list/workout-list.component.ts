c5a95ff6098ef703214c17f7cb25add6c7d42b83
shubhujais15 <shubhujaisedu15@gmail.com>
2024-07-19 02:35:05 +0530

    ngOnInit & applyFilters & removeWorkout functions created

--- src/app/components/workout-list/workout-list.component.ts
+++ src/app/components/workout-list/workout-list.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { WorkoutService, Workout } from '../../services/workout.service';
 
 @Component({
   selector: 'app-workout-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './workout-list.component.html',
-  styleUrl: './workout-list.component.css'
+  styleUrls: ['./workout-list.component.css']
 })
-export class WorkoutListComponent {
+export class WorkoutListComponent implements OnInit {
+  workouts: Workout[] = [];
+  filteredWorkouts: Workout[] = [];
+  searchName = '';
+  filterType = '';
+  page = 1;
+  pageSize = 5;
 
+  constructor(private workoutService: WorkoutService) {}
+
+  ngOnInit() {
+    this.workoutService.workouts$.subscribe(workouts => {
+      this.workouts = workouts;
+      this.applyFilters();
+    });
+  }
+
+  applyFilters() {
+    this.filteredWorkouts = this.workouts.filter(workout =>
+      workout.username.toLowerCase().includes(this.searchName.toLowerCase()) &&
+      (!this.filterType || workout.workoutType === this.filterType)
+    );
+  }
+
+  removeWorkout(userId: number) {
+    this.workoutService.removeWorkout(userId);
+  }
 }
