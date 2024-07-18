9b300838e7612707b4b4467c6bc1bb0c244661f4
shubhujais15 <shubhujaisedu15@gmail.com>
2024-07-18 14:33:38 +0530

    Setup Angular initially

---
+++ src/app/components/add-workout/add-workout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-workout',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-workout.component.html',
+  styleUrl: './add-workout.component.css'
+})
+export class AddWorkoutComponent {
+
+}
