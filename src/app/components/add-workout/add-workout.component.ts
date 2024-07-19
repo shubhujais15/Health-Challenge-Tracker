3bf891e1ba5c24e5ef7affdd468d63ff5c880d94
shubhujais15 <shubhujaisedu15@gmail.com>
2024-07-19 23:30:47 +0530

    Custom workout types option added

--- src/app/components/add-workout/add-workout.component.ts
+++ src/app/components/add-workout/add-workout.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { WorkoutService } from '../../services/workout.service';
+import { WorkoutService, Workout } from '../../services/workout.service';
 
 @Component({
   selector: 'app-add-workout',
@@ -13,23 +13,38 @@ import { WorkoutService } from '../../services/workout.service';
 export class AddWorkoutComponent {
   username = '';
   workoutType = '';
+  customWorkoutType = '';
   workoutMinutes: number | null = null;
+  isCustomWorkout = false;
 
   constructor(private workoutService: WorkoutService) {}
 
   addWorkout() {
-    if (this.username && this.workoutType && this.workoutMinutes) {
-      const newWorkout = {
-        username: this.username,
-        workoutType: this.workoutType,
-        workoutMinutes: this.workoutMinutes
+    if (this.username && this.workoutMinutes) {
+      const workoutType = this.isCustomWorkout ? this.customWorkoutType : this.workoutType;
+      if (!workoutType) return;
+
+      const newWorkout: Workout = {
+        type: workoutType,
+        minutes: this.workoutMinutes
       };
-      this.workoutService.addWorkout(newWorkout);
+
+      this.workoutService.addWorkoutToUser(this.username, newWorkout);
 
       // Reset the form
       this.username = '';
       this.workoutType = '';
+      this.customWorkoutType = '';
       this.workoutMinutes = null;
+      this.isCustomWorkout = false;
+    }
+  }
+
+  checkCustomWorkoutType(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.isCustomWorkout = selectElement.value === 'Other';
+    if (!this.isCustomWorkout) {
+      this.customWorkoutType = '';
     }
   }
 }
